aa5eda1a651175f9d060803fff5ae39d200f7025
Jason Ronda <jason.ronda002@gmail.com>
2019-10-11 15:06:27 +0800

    store token locally

--- src/components/form/signinform.tsx
+++ src/components/form/signinform.tsx
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 import { Formik } from 'formik';
+import axios from 'axios';
 import ErrorBoundary  from '../errorboundary/errorboundary';
 
 export class Signinform extends React.Component {
+  state = {
+    token: []
+  };
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('token')
+      const token = JSON.parse(json)
+
+      if(token) {
+          this.setState(()=> ({ token }))
+      }
+  } catch (e) {}
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+      if(prevState.token.length !== this.state.token.length){
+        const json = JSON.stringify(this.state.token)
+        localStorage.setItem('token', json)
+    }
+  }
 
   render () {
-   
     return (
-      
       <div className="signinform">
           <Formik 
           initialValues= {{username: '', password: ''}}
           onSubmit={(values, {setSubmitting}) =>{
-            
-            setTimeout(()=>{
-              console.log(values);
-              values.username = '';
-              values.password = '';
-              console.log(values);
-              setSubmitting(false)
-            }, 5000)
-          }}
-          validate={(values) =>{
-            let errors = {}
-            if(!values.username){
-              errors.username = 'Please enter a name';
+            const data= {
+              name: values.username,
+              pass: values.password
             }
-            if(!values.password){
-              errors.password = 'Please enter a password';
-            }
-            return errors;
+            axios.post('http://3.94.126.80/user/login?_format=hal_json', data).then((response)=> {
+              this.setState(()=>({
+                token: response.data.csrf_token
+              }))
+              setSubmitting{false}
+            }).catch((e)=>{ })
           }}
           >
             {({handleSubmit, handleChange, values, errors, handleBlur, touched, isSubmitting}) => (
